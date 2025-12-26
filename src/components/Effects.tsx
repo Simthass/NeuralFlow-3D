d28d3ceb45514d4442195cff521be7323d58f25a
simthass <simthass@outlook.com>
2025-12-26 15:21:00 +0530

    fix: Correct EffectComposer prop name

--- src/components/Effects.tsx
+++ src/components/Effects.tsx
@@ -9,24 +9,16 @@ import { BlendFunction } from "postprocessing";
 
 export function Effects() {
   return (
-    <EffectComposer disableNormalPass multisampling={0}>
-      {/* Bloom for the glowing neurons */}
-      {/* @ts-ignore */}
+    <EffectComposer enableNormalPass={false} multisampling={0}>
       <Bloom luminanceThreshold={0.2} mipmapBlur intensity={1.5} radius={0.4} />
 
-      {/* Subtle chromatic aberration for holographic look */}
-      {/* @ts-ignore */}
       <ChromaticAberration
         blendFunction={BlendFunction.NORMAL} // Use NORMAL blend mode
         offset={[0.002, 0.002]} // Correct type: [x, y] vector
       />
 
-      {/* Film grain for realism */}
-      {/* @ts-ignore */}
       <Noise opacity={0.05} />
 
-      {/* Vignette to focus center */}
-      {/* @ts-ignore */}
       <Vignette eskil={false} offset={0.1} darkness={1.1} />
     </EffectComposer>
   );
